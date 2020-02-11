ecbdaa0227860cb019adf5afd982f8d8120fba6c
Eder Canada <canada.eder@gmail.com>
2020-02-11 11:59:46 -0800

    pass parent function to child component

--- src/App.js
+++ src/App.js
@@ -12,17 +12,26 @@ class Product extends Component {
     });
   }
 
+  show = () => {
+    this.props.handleShow(this.props.name)
+  }
+
   render() {
     return (
       <div>
-        <p> {this.props.name} - {this.props.price} </p>
+        <p> {this.props.name} - ${this.props.price}  </p>
         <button onClick={this.buy}> Buy </button>
+        <button onClick={this.show}> Show </button>
         <h3> Qty: {this.state.qty} items(s)</h3>
         <hr/>
       </div>
     );
   }
 }
+
+
+
+
 
 
 class Total extends Component {
@@ -35,20 +44,28 @@ class Total extends Component {
   }
 }
 
+
+
+
+
+
 class ProductList extends Component {
+
+    showProduct = (name) => {
+      alert(`You selected ${name}`)
+    }
+
   render(){
     return(
       <div>
-          <Product name="Andriod" price="121"/>
-          <Product name="Apple" price="123"/>
-          <Product name="nokia" price="59"/>
+          <Product name="Andriod" price="121" handleShow={this.showProduct}/>
+          <Product name="Apple" price="123" handleShow={this.showProduct}/>
+          <Product name="nokia" price="59" handleShow={this.showProduct}/>
           <Total/>
       </div>
     )
   }
 }
-
-
 
 
 export default ProductList;
