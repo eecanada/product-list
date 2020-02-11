1587cb88ae95387fc2848d202592d395213226a2
Eder Canada <canada.eder@gmail.com>
2020-02-11 13:56:24 -0800

    refactor code to render dynamic array

--- src/App.js
+++ src/App.js
@@ -3,80 +3,80 @@ import "./App.css";
 
 class Product extends Component {
   state = {
-      qty: 0
-    }
-  
+    qty: 0
+  };
+
   buy = () => {
     this.setState({
-      qty : this.state.qty + 1
-      
+      qty: this.state.qty + 1
     });
-    this.props.handleTotal(this.props.price)
-  }
+    this.props.handleTotal(this.props.price);
+  };
 
   show = () => {
-    this.props.handleShow(this.props.name) 
-  }
+    this.props.handleShow(this.props.name);
+  };
 
   render() {
     return (
       <div>
-        <p> {this.props.name} - ${this.props.price}  </p>
+        <p>
+          {this.props.name} - ${this.props.price}
+        </p>
         <button onClick={this.buy}> Buy </button>
         <button onClick={this.show}> Show </button>
         <h3> Qty: {this.state.qty} items(s)</h3>
-        <hr/>
+        <hr />
       </div>
     );
   }
 }
 
-
-
-
-
-
 class Total extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h3> Total Cash: ${this.props.total} </h3>
       </div>
-    )
+    );
   }
 }
-
-
-
-
-
 
 class ProductList extends Component {
+  state = {
+    total: 0,
+    productList: [
+      { name: "Andriod", price: 121 },
+      { name: "Apple", price: 123 },
+      { name: "Nokia", price: 59 }
+    ]
+  };
 
-    state = {
-      total: 0
-    }
-  
+  calculateTotal = price => {
+    this.setState({ total: this.state.total + price });
+  };
 
-  calculateTotal = (price) => {
-    this.setState({total: this.state.total + price}) 
-  }
+  showProduct = name => {
+    alert(`You selected ${name}`);
+  };
 
-    showProduct = (name) => {
-      alert(`You selected ${name}`)
-    }
-
-  render(){
-    return(
+  render() {
+    const products = this.state.productList.map((product) => {
+      return (
+        <Product
+          name={product.name}
+          price={product.price}
+          handleShow={this.showProduct}
+          handleTotal={this.calculateTotal}
+        />)
+    });
+    return (
       <div>
-          <Product name="Andriod" price={121} handleShow={this.showProduct} handleTotal={this.calculateTotal}/>
-          <Product name="Apple" price={123} handleShow={this.showProduct} handleTotal={this.calculateTotal}/>
-          <Product name="nokia" price={59} handleShow={this.showProduct} handleTotal={this.calculateTotal}/>
-          <Total total={this.state.total}/>
+        {products}
+        <Total total={this.state.total} />
       </div>
-    )
+    );
   }
 }
-
 
 export default ProductList;
